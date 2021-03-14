6f4ab76c63340ceef40508352f21cbbdc53b25c8
andresmorales84 <80273053+andresmorales84@users.noreply.github.com>
2021-03-13 22:54:56 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -3,15 +3,16 @@ import Dictionary
 print("These are the list of available words to look for: ", Dictionary.d)
 print("The dictionary's length is: ", len(Dictionary.d))
 translatedphrase = {}
-phraseinenglish = []
+phraseinspanish = []
 
-phraseinspanish = (input("Provide the phrase in Spanish (in lower case): "))
-setofwords = phraseinspanish.split(" ")
+
+phraseinenglish = (input("Provide the phrase in English (in lowercase): "))
+setofwords = phraseinenglish.split(" ")
 
 for c in setofwords:
-    phraseinenglish.append(Dictionary.d.get(c))
+    phraseinspanish.append(Dictionary.d.get(c))
 
-print("Phrase in Spanish: ", phraseinspanish, "\nPhrase translated to English: ", *phraseinenglish)
+print("Phrase in English: ", phraseinenglish, "\nPhrase translated to Spanish: ", *phraseinspanish)
 
 
 
